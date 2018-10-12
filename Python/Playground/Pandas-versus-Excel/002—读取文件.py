f2dea78b924d14882a7e4f4731eaf22a132c0dd5
FengLingDu <leao8377@hotmail.com>
2018-10-12 19:20:00 +0800

    002—读取文件   {   1、shape 查看出多少行多少列   2、colums 列名是什么   3、head（x）x条数据，默认为=5   4、tail（n）x条数据，默认为加=5   5、杂乱数据梳理   6、文件内没有列名   7、设置索引【a.set_index；b.inplace】   8、在读取文件时，知道某列为索引的话，可提前指明   }

--- Python/Playground/Pandas-versus-Excel/002—读取文件.py
+++ Python/Playground/Pandas-versus-Excel/002—读取文件.py
@@ -9,3 +9,32 @@ print(people.head(100)) #head(x) 前x条数据，默认为x=5
 print('=========================================')
 print(people.tail(20)) #tail(n) 前x条数据，默认为n=5
 print('Done!')
+
+print('==========================================================')
+#读取excel文件【people】 文件内有杂乱数据，进行梳理
+people2 = pd.read_excel('D:\OneDrive\OneDrive - LingFeng\H-CodingWorld\Python\Playground\Pandas-versus-Excel\Temp\Demo_002_People2.xlsx',header=2) #header=x 忽略前x列，x默认为0
+print(people2.columns)
+
+print('==========================================================')
+#读取excel文件【people】 文件内没有列名
+people3 = pd.read_excel('D:\OneDrive\OneDrive - LingFeng\H-CodingWorld\Python\Playground\Pandas-versus-Excel\Temp\Demo_002_People3.xlsx',header=None)#header=None 读取excel时不要设置header
+
+
+print('==========================================================')
+#人为的设置列名
+people3.columns = ['ID','Type','Title','FirstName','MiddleName','LastName']
+people4 = people3.set_index('ID') #将ID列设置为索引
+#people4 = people3.set_index('ID',inplace=True) #直接改，不生成新的索引
+print(people4.columns)
+people4.to_excel('D:\OneDrive\OneDrive - LingFeng\H-CodingWorld\Python\Playground\Pandas-versus-Excel\Temp\Demo_002_People4.xlsx')
+print('Done!')
+
+
+print('==========================================================')
+#在读取文件时，知道某列为索引的话，可提前指明
+df = pd.read_excel('D:\OneDrive\OneDrive - LingFeng\H-CodingWorld\Python\Playground\Pandas-versus-Excel\Temp\Demo_001_output2.xlsx',index_col='ID')
+df.to_excel('D:\OneDrive\OneDrive - LingFeng\H-CodingWorld\Python\Playground\Pandas-versus-Excel\Temp\Demo_001_output3.xlsx')
+print(df.head())
+
+
+
